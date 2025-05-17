351544d7f1f4ad52cce57cdf183e6fdbba7f954b
tranhoainam <21130450@st.hcmuaf.edu.vn>
2025-05-17 15:03:22 +0700

    export public key and export private key

--- BackEnd/src/main/java/com/example/backend/utils/CryptoUtil.java
+++ BackEnd/src/main/java/com/example/backend/utils/CryptoUtil.java
@@ -3,6 +3,7 @@ package com.example.backend.utils;
 import java.io.*;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
@@ -19,6 +20,17 @@ public class CryptoUtil {
     }
     public void exportPublicKey(String fileName) throws FileNotFoundException {
         PrintWriter pw = new PrintWriter(new FileOutputStream(fileName));
+        pw.println("-----BEGIN PUBLIC KEY-----");
+        String key = Base64.getEncoder().encodeToString(publicKey.getEncoded());
+        for(int i = 0; i < key.length(); i += 64){
+            int end = Math.min(i + 64, key.length());
+            pw.println(key.substring(i, end));
+        }
+        pw.println("-----END PRIVATE KEY-----");
+        pw.close();
+    }
+    public void exportPrivateKey(String filename) throws FileNotFoundException {
+        PrintWriter pw = new PrintWriter(new FileOutputStream(filename));
         pw.println("-----BEGIN PRIVATE KEY-----");
         String key = Base64.getEncoder().encodeToString(privateKey.getEncoded());
         for(int i = 0; i < key.length(); i += 64){
@@ -27,8 +39,9 @@ public class CryptoUtil {
         }
         pw.println("-----END PRIVATE KEY-----");
         pw.close();
+
     }
-    public void importPublic (String fileName) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+    public void importPublicKey (String fileName) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
         String line;
         String key="";
@@ -41,5 +54,20 @@ public class CryptoUtil {
         X509EncodedKeySpec keySpec = new X509EncodedKeySpec(encoded);
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
         this.publicKey = keyFactory.generatePublic(keySpec);
+    }
+    public void importPrivateKey (String fileName) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+        String line;
+        String key="";
+
+        while((line = br.readLine())!= null){
+            if(line.contains("PRIVATE kEY")) continue;
+            key += line;
+        }
+
+        byte [] encoded = Base64.getDecoder().decode(key);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(encoded);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        this.privateKey = keyFactory.generatePrivate(keySpec);
     }
 }
