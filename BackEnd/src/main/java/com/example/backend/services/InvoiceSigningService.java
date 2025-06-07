735a0b5d16765af20096f32107abe25f87495f30
HoaiBaoHong <21130281@st.hcmuaf.edu.vn>
2025-06-08 00:39:00 +0700

    add generateInvoicecData method -> chuan hoa du lieu thanh chuoi, phan cach du lieu

--- BackEnd/src/main/java/com/example/backend/services/InvoiceSigningService.java
+++ BackEnd/src/main/java/com/example/backend/services/InvoiceSigningService.java
@@ -1,18 +1,21 @@
 package com.example.backend.services;
 
 import com.example.backend.models.Invoice;
-import java.security.KeyFactory;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
 import com.example.backend.utils.RSASignatureUtil;
 
 import java.security.KeyPair;
 import java.util.Date;
 
 public class InvoiceSigningService {
+
+    public static String generateInvoiceData(Invoice invoice) {
+        return invoice.getMaDonHang() + "|" +
+                invoice.getMaKhachHang() + "|" +
+                invoice.getTongTien();
+    }
+
     public void signInvoice(Invoice invoice, KeyPair keyPair) throws Exception {
-        String data = invoice.getMaDonHang() + invoice.getMaKhachHang() + invoice.getTongTien();
+        String data = generateInvoiceData(invoice);
         String signature = RSASignatureUtil.sign(data, keyPair.getPrivate());
         invoice.setDigitalSignature(signature);
         invoice.setSignedAt(new Date());
